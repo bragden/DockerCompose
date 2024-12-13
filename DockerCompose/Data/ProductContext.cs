094f31cedabd0f089babf0069f4cc9802bacbb05
Oliver Brage <Brage.Oliver@hotmail.com>
2024-12-13 14:01:19 +0100

    Add project files.

---
+++ DockerCompose/Data/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DockerCompose.Models;
+
+namespace DockerCompose.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
